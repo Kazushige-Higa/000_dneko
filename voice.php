bd37d04cfa7fcbb9fa053cbb99fe6e6691bf5c8b
ガーヒー <newlife.kazu8131@gmail.com>
2026-08-03 07:30:51 +0900

    トップページとお客様の声を改善

--- voice.php
+++ voice.php
@@ -3,7 +3,7 @@ $page_title = "お客様の声";
 $page_title_eng = "Voice";
 $page_seo_title = "お客様の声｜沖縄のデザイン・ホームページ制作";
 $page_description = "デザネコへチラシやホームページ制作をご依頼いただいた沖縄のお客様の声をご紹介します。相談時の対応、制作過程、完成後の変化をご覧ください。";
-$page_style = "";
+$page_style = '<link href="css/voice.css?v=' . filemtime(__DIR__ . '/css/voice.css') . '" rel="stylesheet">';
 $page_script = '';
 ?>
 <?php include_once './header.php'; ?>
@@ -11,25 +11,114 @@ $page_script = '';
 
 <!-- ・お客様の声
 voice.php -->
-<div class='overflow'>
-
+<div class='overflow voice_page'>
 
     <section>
-        <div class='bg_base'>
+        <div>
             <div class='single02'>
-                <div class='mbox '>
-                    <div class="tcenter b_m5">
-                        <img width="80px" src='<?php echo $img; ?>/favicon.png' alt='イメージ画像' loading='lazy'>
-                    </div>
+                <div class='mbox'>
                     <h2 class="line_height_14 tcenter">
-                        <span class="eng base_color fs_40 act txt_split type_popup">Voice
-                        </span><br>
                         <span class="fs_40 fs_sp30 act blur font_kiwi">
                             お客様からのうれしい声
                         </span>
                     </h2>
                     <div class='space_3 space_sp3'></div>
                     <ul class="grid2 voice_box">
+                        <li class="b_m20 radius">
+                            <div class='mbox bg_white'>
+                                <div class='flexbox gap3'>
+                                    <div class="width_3 width_sp10">
+                                        <div class="picture">
+                                            <img src='<?php echo $img; ?>/voice-yukyunotabi-ura.jpg' alt='旅行代理店 悠久の旅 沖縄 代表の宇良さま' loading='eager' fetchpriority='high' width='480' height='480'>
+                                        </div>
+                                    </div>
+                                    <div class='width_7 width_sp10 act inup'>
+                                        <p class="bold b_m10 fs_30 fs_sp22">
+                                            <span class="base_color border_bottom">旅行代理店「悠久の旅 沖縄」代表 宇良さま</span><br>
+                                        </p>
+
+                                        <dl class="dl_list radius b_m10">
+                                            <dt>お名前</dt>
+                                            <dd>代表 宇良さま</dd>
+                                            <dt>業種</dt>
+                                            <dd>旅行代理店</dd>
+                                            <dt>地域</dt>
+                                            <dd>沖縄</dd>
+                                            <dt>ホームページ</dt>
+                                            <dd>
+                                                <a class="voice_site_link" href="https://yukyu-okinawa.com/" target="_blank" rel="noopener" aria-label="悠久の旅 沖縄のホームページ（新しいタブで開きます）">
+                                                    悠久の旅 沖縄 <i class="fa-solid fa-arrow-up-right-from-square" aria-hidden="true"></i>
+                                                </a>
+                                            </dd>
+                                        </dl>
+
+                                        <p class="voice_quote">
+                                            「“本能にぶっ刺さるチラシ”で受注！ホームページでさらに魅力を発信中！」
+                                        </p>
+
+                                        <div class="memo">
+                                            <p>
+                                                「沖縄2泊3日の旅」の魅力を伝えるため、「本能にぶっ刺さるデザインを」とオーダーしました。
+                                            </p>
+                                            <p>
+                                                完成したチラシは、文字通り本能を揺さぶる圧倒的なクオリティ！お客様にお見せすると即決でご契約となり、旅行自体も大好評でした。
+                                            </p>
+                                            <p>
+                                                さらに第2弾の受注へと展開しました。ホームページと印刷物を一貫して任せられるので、ブランドに統一感を持たせられることの大きな力を実感しています！
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="b_m20 radius">
+                            <div class='mbox bg_white'>
+                                <div class='flexbox gap3'>
+                                    <div class="width_3 width_sp10">
+                                        <div class="picture">
+                                            <img class="voice_portrait_top" src='<?php echo $img; ?>/voice-nicoli-arakaki.jpg' alt='ニコリミュージック代表 新垣里実さま' loading='lazy' width='480' height='720'>
+                                        </div>
+                                    </div>
+                                    <div class='width_7 width_sp10 act inup'>
+                                        <p class="bold b_m10 fs_30 fs_sp22">
+                                            <span class="base_color border_bottom">ニコリミュージック 代表 新垣里実さま</span><br>
+                                        </p>
+
+                                        <dl class="dl_list radius b_m10">
+                                            <dt>お名前</dt>
+                                            <dd>代表 新垣里実さま</dd>
+                                            <dt>教室名</dt>
+                                            <dd>ニコリミュージック</dd>
+                                            <dt>ホームページ</dt>
+                                            <dd>
+                                                <a class="voice_site_link" href="https://nicoli-music.com/" target="_blank" rel="noopener" aria-label="ニコリミュージックのホームページ（新しいタブで開きます）">
+                                                    ニコリミュージック <i class="fa-solid fa-arrow-up-right-from-square" aria-hidden="true"></i>
+                                                </a>
+                                            </dd>
+                                        </dl>
+
+                                        <p class="voice_quote">
+                                            「ホームページとチラシが、毎年安定したお問い合わせにつながっています」
+                                        </p>
+
+                                        <div class="memo">
+                                            <p>
+                                                ホームページとチラシを制作していただいてから、お問い合わせが毎年継続的に入るようになりました。
+                                            </p>
+                                            <p>
+                                                以前は口コミ中心で、教室周辺からのお問い合わせがほとんどでしたが、現在では少し離れた地域からも、ホームページをご覧になってご連絡をいただいています。
+                                            </p>
+                                            <p>
+                                                昨年度は年間約60件、今年度も10月から現在までに45件のお問い合わせがあり、そのうち34名の方にご入会いただいています。
+                                            </p>
+                                            <p>
+                                                教室の魅力が分かりやすく伝わるホームページとチラシのおかげで、毎年安定した集客につながっていると実感しています。
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
                         <li class="b_m20 radius">
                             <div class='mbox bg_white'>
                                 <div class='flexbox gap3'>
