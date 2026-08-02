03caf14021698f28863a89e79d75d2b25ef2e005
ガーヒー <newlife.kazu8131@gmail.com>
2026-08-02 12:24:16 +0900

    Renew shared page titles

--- page_title.php
+++ page_title.php
@@ -1,26 +1,123 @@
-<div class="overflow">
-    <div class="page_title">
-        <div class="puton l1 b2">
-            <div class='space_1 space_sp4'></div>
-            <h2 class="line_height_14">
-                <span class="act01 txt_split type_up fs_25 fs_sp25 base_color eng shadow"><?php echo $page_title_eng; ?></span><br>
-                <span class="act02 txt_split type_up fs_60 fs_sp35 base_color shadow font_notob"><?php echo $page_title; ?></span><br>
-            </h2>
-        </div>
-        <div class="puton r1 b1 pconly tright width_2">
-            <ol class="breadcrumb">
-                <li><a href="./" class="home">TOP</a></li>
-                <li><?php echo $page_title; ?></li>
+<?php
+$dr_page_title_key = strtolower(trim((string) ($page_title_eng ?? '')));
+$dr_page_title_variants = [
+    '404' => [
+        'icon' => 'fa-solid fa-triangle-exclamation',
+        'left' => '53.webp',
+        'right' => '52.webp',
+    ],
+    'about us' => [
+        'icon' => 'fa-solid fa-cat',
+        'left' => '49.webp',
+        'right' => '48.webp',
+    ],
+    'contact' => [
+        'icon' => 'fa-solid fa-envelope',
+        'left' => '57.webp',
+        'right' => '56.webp',
+    ],
+    'portfolio' => [
+        'icon' => 'fa-solid fa-images',
+        'left' => '69.webp',
+        'right' => '41.webp',
+    ],
+    'works archive' => [
+        'icon' => 'fa-solid fa-images',
+        'left' => '69.webp',
+        'right' => '41.webp',
+    ],
+    'column' => [
+        'icon' => 'fa-solid fa-lightbulb',
+        'left' => '61.webp',
+        'right' => '58.webp',
+    ],
+    'archive' => [
+        'icon' => 'fa-solid fa-box-archive',
+        'left' => '61.webp',
+        'right' => '58.webp',
+    ],
+    'news' => [
+        'icon' => 'fa-solid fa-newspaper',
+        'left' => '61.webp',
+        'right' => '58.webp',
+    ],
+    'faq' => [
+        'icon' => 'fa-solid fa-circle-question',
+        'left' => '09.webp',
+        'right' => '07.webp',
+    ],
+    'law' => [
+        'icon' => 'fa-solid fa-file-contract',
+        'left' => '15.webp',
+        'right' => '14.webp',
+    ],
+    'privacy policy' => [
+        'icon' => 'fa-solid fa-shield-halved',
+        'left' => '15.webp',
+        'right' => '14.webp',
+    ],
+    'profile' => [
+        'icon' => 'fa-solid fa-user-pen',
+        'left' => '49.webp',
+        'right' => '48.webp',
+    ],
+    'service design' => [
+        'icon' => 'fa-solid fa-pen-ruler',
+        'left' => '70.webp',
+        'right' => '03.webp',
+    ],
+    'voice' => [
+        'icon' => 'fa-solid fa-comments',
+        'left' => '42.webp',
+        'right' => '41.webp',
+    ],
+];
+
+$dr_page_title_variant = $dr_page_title_variants[$dr_page_title_key] ?? [
+    'icon' => 'fa-solid fa-paw',
+    'left' => '42.webp',
+    'right' => '41.webp',
+];
+
+$dr_page_title_icon = $page_title_icon ?? $dr_page_title_variant['icon'];
+$dr_page_title_left = $page_title_sticker_left ?? $dr_page_title_variant['left'];
+$dr_page_title_right = $page_title_sticker_right ?? $dr_page_title_variant['right'];
+$dr_sticker_directory = rtrim((string) ($img ?? 'images'), '/') . '/sticker/';
+$dr_page_title_id = 'dr-page-title-' . substr(md5((string) ($page_title_eng ?? $page_title ?? 'page')), 0, 8);
+
+$dr_page_title_escape = static function ($value) {
+    return htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
+};
+?>
+<section class="dr_page_title" aria-labelledby="<?= $dr_page_title_escape($dr_page_title_id); ?>">
+    <div class="dr_page_title_pattern" aria-hidden="true"></div>
+
+    <img
+        class="dr_page_title_mascot dr_page_title_mascot_left"
+        src="<?= $dr_page_title_escape($dr_sticker_directory . $dr_page_title_left); ?>"
+        alt=""
+        width="200"
+        height="200">
+
+    <div class="dr_page_title_inner">
+        <span class="dr_page_title_icon" aria-hidden="true">
+            <i class="<?= $dr_page_title_escape($dr_page_title_icon); ?>"></i>
+        </span>
+        <p class="dr_page_title_eng" lang="en"><?= $dr_page_title_escape($page_title_eng ?? ''); ?></p>
+        <h1 class="dr_page_title_jp" id="<?= $dr_page_title_escape($dr_page_title_id); ?>"><?= $dr_page_title_escape($page_title ?? ''); ?></h1>
+
+        <nav class="dr_page_title_breadcrumb" aria-label="パンくずリスト">
+            <ol>
+                <li><a href="./">ホーム</a></li>
+                <li aria-current="page"><?= $dr_page_title_escape($page_title ?? ''); ?></li>
             </ol>
-        </div>
-        <div class="photo">
-            <img loading="lazy" src="<?php
-                                        if (!empty($page_title_img)) {
-                                            echo $page_title_img;
-                                        } else {
-                                            echo $page_images;
-                                        }
-                                        ?>" width="100%" alt="<?php echo $company; ?>イメージ画像">
-        </div>
+        </nav>
     </div>
-</div>
+
+    <img
+        class="dr_page_title_mascot dr_page_title_mascot_right"
+        src="<?= $dr_page_title_escape($dr_sticker_directory . $dr_page_title_right); ?>"
+        alt=""
+        width="200"
+        height="200">
+</section>
