b244162865d93054e9c27133bfea65917f560393
ガーヒー <newlife.kazu8131@gmail.com>
2026-09-19 05:08:28 +0900

    Refine blog article typography and hide lower service banners

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<?php include __DIR__ . '/service-banners.php'; ?>
+<?php if (empty($hide_service_banners)) include __DIR__ . '/service-banners.php'; ?>
 <?php if (empty($hide_footer_contact)) include __DIR__ . '/footer-contact.php'; ?>
 <?php require_once __DIR__ . '/site-navigation.php'; ?>
 <?php
